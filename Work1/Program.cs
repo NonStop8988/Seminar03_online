f88a8eae1fa659df7b967ce09c28f50b41aad006
Maxim <lyskomax2@mail.ru>
2024-01-11 21:51:51 +0300

    Add Work1

---
+++ Work1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 1, 2, 3, 4, 5 };
+int target = 5;
+
+bool flag = false; // флаг опущен (не посещался, нет такого элемента в массиве)
+
+foreach (int elem in array)
+{
+    if (elem == target)
+    {
+        // Console.WriteLine(elem);
+        flag = true;
+    }
+}
+
+if (flag == true)
+{
+    Console.WriteLine($"Присутствует");
+}
+else
+{
+    Console.WriteLine($"Не присутствует");
+}
