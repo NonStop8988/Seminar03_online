4cc3655ad58b7ab600782cb364b5792697fa8ead
Maxim <lyskomax2@mail.ru>
2024-01-14 13:13:10 +0300

    Adjustment after verification

--- Work4/Program.cs
+++ Work4/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, состоящий из цифр этого числа. Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. Размер массива должен быть равен количеству цифр.
 
-int number = 100000;
+int number = 5498;
 int tempNumber = number;
-int listNumber = 0;
+int digitsCount = 0;
 
 if (number > 0 && number < 100000)
 {
     while (tempNumber > 0)
     {
         tempNumber /= 10;
-        listNumber++;
+        digitsCount++;
     }
-    // Console.WriteLine(listNumber);
+    // Console.WriteLine(digitsCount);
 
-    int[] arr = new int[listNumber];
+    int[] arr = new int[digitsCount];
     tempNumber = number;
 
-    for (int i = listNumber - 1; i >= 0; i--)
+    for (int i = digitsCount - 1; i >= 0; i--)
     {
         arr[i] = tempNumber % 10;
         tempNumber /= 10;
